f6254c14e718724b2a44e4be30034343c7218eb2
Hugo CAPOCCI <hcapocci@degetel.com>
2018-04-11 15:00:51 +0200

    Add test for promiseBatch

--- test.js
+++ test.js
@@ -1,19 +1,32 @@
-import { promiseEach } from './index';
+import { promiseEach, promiseBatch } from './index';
+
+const testPromise =  (value) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('resolve value ', value);
+      resolve(value);
+    }, 1000);
+  });
+}
 
 describe('promiseEach', () => {
   it('Should resolve promise one by one', (done) => {
     const values = [1, 2, 3, 4, 5, 6];
-    promiseEach(values, (value) => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          console.log('resolve value ', value);
-          resolve(value);
-        }, 1000);
-      });
-    })
+    promiseEach(values, testPromise)
     .then((results) => {
       expect(results).toEqual(values);
       done();
     });
   }, 10000);
+});
+
+describe('promiseBatch', () => {
+  it('Should execute batch of promises in series', (done) => {
+    const values = [1, 2, 3, 4, 5, 6];
+    promiseBatch(values, testPromise, 2)
+    .then((results) => {
+      expect(results).toEqual(values);
+      done();
+    });
+  }, 5000);
 });
